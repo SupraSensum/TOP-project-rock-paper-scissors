fb4ed1c07b3b84ba1e3b4cb16e01c9287697d23d
SupraSensum <107372360+SupraSensum@users.noreply.github.com>
2023-07-31 23:46:08 -0300

    Create getComputerChoice function

---
+++ rock-paper-scissors.js
@@ -0,0 +1,16 @@
+function getComputerChoice() {
+    switch (Math.floor(Math.random() * 3) + 1) {
+        case 1:
+            return "Rock";
+            break;
+        case 2:
+            return "Paper";
+            break;
+        case 3: 
+            return "Scissors";
+            break;
+        default:
+            console.error("You shouldn't be here");
+            break;
+    }
+}
